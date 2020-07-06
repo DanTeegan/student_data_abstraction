aac63aab16952e2a82e25c1fe86afd6cbb48b541
dan. teegan <dan.teegan@hotmail.com>
2020-07-06 15:46:54 +0100

    Abstraction

---
+++ devops_student.py
@@ -0,0 +1,15 @@
+from student_data import Student
+
+
+class Devops(Student):
+    def __init__(self, first_name, last_name, stream): # This adds an attribute "stream" to the devops sub class.
+        self.first_name = first_name
+        self.last_name = last_name
+        self.stream = stream
+
+    def full_name(self):
+        print(self.first_name, self.last_name)
+
+    def roll_call(self):
+        print("I am a Devops Student")
+
